4894f92a1640b2b693bb8119df474c4cbcd1e472
Lin <llan.xjtu@foxmail.com>
2017-03-01 21:01:00 +0800

    initial commit

---
+++ console.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 16:46:10 2016
+
+@author: lanlin
+"""
+
+import os
+import argparse
+import yaml
+
+def main(args):
+    with open(args.config, "r") as f:
+        config = yaml.load(f)
+    try:
+        k8s_config = config["kubernetes"]
+    except KeyError:
+        k8s_config = {}
+    try:
+        tf_config = config["tensorflow"]
+    except KeyError:
+        tf_config = {}
+    try:
+        data_config = config["data"]
+    except KeyError:
+        data_config = {}
+    
+    num_gpus = k8s_config["num_gpus"]
+    k8s_config.pop("num_gpus")
+    k8s_config["ps_replicas"] = num_gpus \
+        if num_gpus % 2 != 0 else num_gpus - 1
+    k8s_config["worker_replicas"] = num_gpus
+    
+    k8s_args = ["--{} \"{}\"".format(key, value)
+        for key, value in k8s_config.iteritems()]
+    k8s_args = " ".join(k8s_args) if k8s_args else ""
+    
+    tf_args = [(key, value) for key, value in tf_config.iteritems()]
+    tf_args = "--parameters \"{}\"".format(tf_args) if tf_args else ""
+    render_args = "{} {}".format(k8s_args, tf_args)
+    
+    os.system("python ./kubernetes/render.py {}".format(render_args))
+    copy_files = ["./tensorflow/distribute_tf.py"]
+    if "user_model_file" not in tf_config.keys() \
+        or tf_config["user_model_file"] == "example_user_model.py":
+        copy_files.append("./examples/example_user_model.py")
+    else:
+        copy_files.append(tf_config["user_model_file"])
+    if "data_dir" not in data_config.keys() \
+        or data_config["data_dir"] == "./examples/data":
+        copy_files.append("./examples/data")
+    else:
+        copy_files.append(data_config["data_dir"])
+
+    work_dir = ROOT_DIR + "../gpu_cluster_storage/{}/{}".format(
+                                                k8s_config["namespace"],
+                                                k8s_config["name"])
+
+    os.system("mkdir -p {}".format(work_dir))
+    os.system("cp -r {} {}".format(" ".join(copy_files), work_dir))
+    os.system("chmod +x {}/start_job.sh".format(work_dir))
+    os.system("chmod +x {}/stop_job.sh".format(work_dir))
+#    os.system("chmod +x {}/start_tensorboard.sh".format(work_dir))
+#    os.system("chmod +x {}/stop_tensorboard.sh".format(work_dir))
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="")
+        
+    parser.add_argument(
+        "--config",
+        default="./examples/example_config.yaml")
+    
+    args = parser.parse_args()
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    ROOT_DIR = "{}/".format(os.getcwd())
+    config=main(args)
---
+++ kubernetes/render.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 20:18:45 2016
+
+@author: lanlin
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import argparse
+import jinja2
+
+NAMESPACE_JINJA = "namespace.jinja"
+NFS_JINJA = "nfs.jinja"
+CLUSTER_SERVICE_JINJA = "cluster_svc.jinja"
+CLUSTER_GROUP_JINJA = "cluster_job.jinja"
+#NFS_TENSORBOARD_JINJA = "nfs_tensorboard.jinja"
+#TENSORBOARD_JINJA = "tensorboard.jinja"
+START_CLUSTER_JINJA = "start_job.jinja"
+STOP_CLUSTER_JINJA = "stop_job.jinja"
+#START_TENSORBOARD_JINJA = "start_tb.jinja"
+#STOP_TENSORBOARD_JINJA = "stop_tb.jinja"
+
+JINJA_FILE_0 = [NAMESPACE_JINJA, NFS_JINJA,
+                CLUSTER_SERVICE_JINJA, CLUSTER_GROUP_JINJA]
+#                NFS_TENSORBOARD_JINJA, TENSORBOARD_JINJA]
+JINJA_FILE_1 = [START_CLUSTER_JINJA, STOP_CLUSTER_JINJA]
+#                START_TENSORBOARD_JINJA, STOP_TENSORBOARD_JINJA]
+
+def main(args):
+    properties = dict(args._get_kwargs())
+    dirname = ROOT_DIR + "../gpu_cluster_storage/{}/{}/k8s_config".format(
+        properties["namespace"], properties["name"])
+    os.system("mkdir -p {}".format(dirname))
+    for jinja_file in JINJA_FILE_0:
+        extension = "yaml"
+        with open(jinja_file, "r") as f:
+            template = jinja2.Template(f.read())
+        with open("{}/{}.{}".format(
+            dirname, os.path.splitext(jinja_file)[0], extension), "w") as f:
+            f.write(template.render(properties=properties))
+
+    for jinja_file in JINJA_FILE_1:
+        extension = "sh"
+        with open(jinja_file, "r") as f:
+            template = jinja2.Template(f.read())
+        with open("{}/{}.{}".format(
+            os.path.dirname(dirname),
+            os.path.splitext(jinja_file)[0], extension), "w") as f:
+            f.write(template.render(properties=properties))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="")
+    
+    parser.add_argument(
+        "--namespace",
+        default="llan")
+    parser.add_argument(
+        "--name",
+        default="train")
+    parser.add_argument(
+        "--tf_image",
+        default="tensorflow/tf-cluster-train:3.0",
+        required=False)
+    parser.add_argument(
+        "--ps_replicas",
+        type=int,
+        required=True)
+    parser.add_argument(
+        "--worker_replicas",
+        type=eval,
+        required=True)
+    parser.add_argument(
+        "--parameters",
+        default="[]",
+        type=eval)
+    parser.add_argument(
+        "--job_port",
+        default="2222")
+#    parser.add_argument(
+#        "--tensorboard_port",
+#        default="6006")
+    parser.add_argument(
+        "--nvidia_driver_version",
+        default="375.26")
+    parser.add_argument(
+        "--nfs_image",
+        default="gcr.io/google-samples/nfs-server:1.2")
+        
+    args = parser.parse_args()
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    ROOT_DIR = "{}/../".format(os.getcwd())
+    main(args)
